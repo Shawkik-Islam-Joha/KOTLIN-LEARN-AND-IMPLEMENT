432e6392de0ca22ffd56b70ade6facc37e86de8d
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-07-04 23:11:25 +0600

    New Code

---
+++ Basics/3_User_Input.kt
@@ -0,0 +1,7 @@
+fun main(){
+    println("Enter Your Name:")
+    var name = readLine()
+    // readLine() takes user input (as long as Enter key not Hit!) in terminal and stores in name variable
+
+    println("Hello $name")
+}
