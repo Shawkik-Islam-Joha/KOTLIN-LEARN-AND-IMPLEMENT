461d86acdf703365879f37bacb529128b65ce9d7
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-07-04 22:51:36 +0600

    First Commit

---
+++ Basics/1_Hello_World.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("Hello, World!")
+}
+
+// This is a simple Kotlin program that prints "Hello, World!" to the console.
+// It serves as a basic introduction to Kotlin syntax and structure.
