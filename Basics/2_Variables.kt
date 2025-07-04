933dfd350a98a047680854ef12534a29e3d4b09a
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-07-04 23:06:18 +0600

    New Code

--- Basics/2_Variables.kt
+++ Basics/2_Variables.kt
@@ -1,4 +1,16 @@
 fun main(){
     var name = "Joha"   // Variable type, can modify
     val pi = 3.1416     // Value type, can't modify once assigned
+
+    // Kotlin can automatically detect data types, but you can also define them
+    var age: Int = 22
+
+    println("Hello my name is $name and I am $age years old.")
+    // Prints using variables
+
+    var a = 12
+    var b = 24
+    var sum = a+b   // Storing value of addition
+    println(sum)    // Print sum using variable
+    println(a+b)    // Print sum directly
 }
